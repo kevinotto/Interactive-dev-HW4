44299a762d4f10e900fd854eb02048775c23777b
Kevin Otto Koit <ottokoit@gmail.com>
2025-03-10 16:38:52 +0200

    Added images to portfolio

--- PortfolioApp/PortfolioApp/src/components/Projects.jsx
+++ PortfolioApp/PortfolioApp/src/components/Projects.jsx
@@ -1,38 +1,48 @@
 import { useState } from "react";
 
 const Projects = () => {
-  const [showProjects, setShowProjects] = useState(true);
+  const [showProjects, setShowProjects] = useState(false);
 
   const projectList = [
-    { id: 1, name: "OS startup screen" },
-    { id: 2, name: "Unreal Tallinn workshop poster" },
-    { id: 3, name: "TF2 72 hr Jam 3D composition" },
-    { id: 4, name: "Messenger app UI" },
-    { id: 5, name: "Shotgun model" },
-    { id: 6, name: "'VIBRANT' poster" },
-    { id: 7, name: "'PAC-MAN' poster" },
-    { id: 8, name: "3D Rolling ball game" },
-    { id: 9, name: "Educational coding game" },
-    { id: 10, name: "Frutiger Aero human models" },
-    { id: 11, name: "'Godomachy' level design" },
-    { id: 12, name: "Abstract snake game 'NEOQ'" },
-    { id: 13, name: "Frutiger Aurora glades 1" },
-    { id: 14, name: "Frutiger Aurora glades 2" },
-    { id: 15, name: "'ROCKET' poster" },
+    { id: 1, img: "/Project images/Fruitger Aurora PC login screen 1.png", name: <a href="https://www.artstation.com/artwork/rJxWLO" target="_blank" rel="noopener noreferrer"> Frutiger Aurora OS startup screen </a> },
+    { id: 2, img: "/Project images/Unreal Tallinn 2024 Workshop.png", name: <a href="https://www.artstation.com/artwork/eRPLkZ" target="_blank" rel="noopener noreferrer"> Unreal Tallinn workshop poster </a> },
+    { id: 3, img: "/Project images/TF2 mercs voxelheadz 72hr jam.png", name: <a href="https://www.artstation.com/artwork/qJO46P" target="_blank" rel="noopener noreferrer"> TF2 3D composition </a> },
+    { id: 4, img: "/Project images/frutiger aurora messenger app 5.PNG", name: <a href="https://www.artstation.com/artwork/lGDVNa" target="_blank" rel="noopener noreferrer"> Frutiger Aurora messenger app UI</a> },
+    { id: 5, img: "/Project images/Shotgun and ammo showcase10.png", name: <a href="https://www.artstation.com/artwork/KePymr" target="_blank" rel="noopener noreferrer"> 3D shotgun model</a> },
+    { id: 6, img: "/Project images/Vibrant Vibes.png", name: <a href="https://www.artstation.com/artwork/OvyKlK" target="_blank" rel="noopener noreferrer"> VIBRANT VIBES poster</a> },
+    { id: 7, img: "/Project images/pacman green acid.png", name: <a href="https://www.artstation.com/artwork/RKYZ0y" target="_blank" rel="noopener noreferrer"> PAC-MAN green acid style poster</a> },
+    { id: 8, img: "/Project images/AA1.png", name: <a href="https://www.artstation.com/artwork/n0ArX1" target="_blank" rel="noopener noreferrer"> Aenigma Abstractum game</a> },
+    { id: 9, img: "/Project images/HelloWorld main menu UI 4.jpg", name: <a href="https://www.artstation.com/artwork/49K4oY" target="_blank" rel="noopener noreferrer"> Hello World! main menu UI</a> },
+    { id: 10, img: "/Project images/Frutiger pals.png", name: <a href="https://www.artstation.com/artwork/aov9b9" target="_blank" rel="noopener noreferrer"> Frutiger pals</a> },
+    { id: 11, img: "/Project images/Godomachy level design.jpg", name: <a href="https://www.artstation.com/artwork/zD9J3Q" target="_blank" rel="noopener noreferrer"> Godomachy level design</a> },
+    { id: 12, img: "/Project images/NEOQ game.jpg", name: <a href="https://www.artstation.com/artwork/n04qae" target="_blank" rel="noopener noreferrer"> NEOQ game</a> },
+    { id: 13, img: "/Project images/Frutiger Aurora 1.png", name: <a href="https://www.artstation.com/artwork/Jvogka" target="_blank" rel="noopener noreferrer"> Frutiger Aurora glades 1</a> },
+    { id: 14, img: "/Project images/Frutiger Aurora 2.png", name: <a href="https://www.artstation.com/artwork/kNE3bx" target="_blank" rel="noopener noreferrer"> Frutiger Aurora glades 2</a> },
+    { id: 15, img: "/Project images/minimalist rocket.png", name: <a href="https://www.artstation.com/artwork/zOGAxL" target="_blank" rel="noopener noreferrer"> ROCKET poster</a> },
   ];
 
   return (
     <section id="projects">
       <h2>My projects</h2>
       <button onClick={() => setShowProjects(!showProjects)}>
-        {showProjects ? "Hide Projects" : "Show Projects"}
+        {showProjects ? "Hide projects" : "Show projects"}
       </button>
+
       {showProjects && (
-        <ul>
+        <div className="project-container">
           {projectList.map((project) => (
-            <li key={project.id}>{project.name}</li>
+            <div key={project.id} className="project-item">
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                <img src={project.img} alt={project.name} className="project-image" style={{ width: "700px", height: "auto", borderRadius: "5px" }} />
+              </a>
+              <h3>
+                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                  {project.name}
+                </a>
+              </h3>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </section>
   );
